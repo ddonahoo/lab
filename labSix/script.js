3650ecf0c9c562151cc9ed92615a71dedded6576
Deanna <dmdonahoo@gmail.com>
2017-02-08 18:57:07 -0500

    temperature conversion

---
+++ labSix/script.js
@@ -0,0 +1,33 @@
+function fToC (fahrenheit)
+{
+var fTemp = fahrenheit;
+cTemp = (fTemp-32)/1.8;
+console.log(cTemp);
+}
+fToC (40);
+
+function cToF (celsius)
+{
+var cTemp = celsius;
+fTemp = (cTemp*1.8)+32;
+console.log(fTemp);
+}
+cToF(40);
+
+function fToC (fahrenheit)
+{
+var fTemp = fahrenheit;
+var cTemp = (fTemp-32)/1.8;
+var message = cTemp + " degrees celsius is equal to " + fTemp + " degrees fahrenheit.";
+console.log(message);
+}
+fToC (40);
+
+function cToF (celsius)
+{
+var cTemp = celsius;
+var fTemp = (cTemp*1.8)+32;
+var message = fTemp + " degrees fahrenheit is equal to " + cTemp + " degrees celsius.";
+console.log(message);
+}
+cToF (40);
